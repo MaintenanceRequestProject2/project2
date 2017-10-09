ac8e64eed7ad42a2697ed3da8dac18b24511d1f2
Will <will93hump@gmail.com>
2017-10-08 22:23:41 -0400

    generic comment

--- Maintenance/UserUI/Startup.cs
+++ Maintenance/UserUI/Startup.cs
@@ -21,6 +21,7 @@ namespace UserUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add MVC Service
             services.AddMvc();
         }
 
