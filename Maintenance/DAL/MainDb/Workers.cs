920ab117780695f3b10e92fd2e6fd00adaa51a3d
Will <will93hump@gmail.com>
2017-10-09 13:19:55 -0400

    pulled in database from entity framework

---
+++ Maintenance/DAL/MainDb/Workers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdminUI.MainDb
+{
+    public partial class Workers
+    {
+        public int Id { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
