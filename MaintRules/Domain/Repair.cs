cfcafc2ea5049e31eb27b190023d32e0ec89e990
aoscovitch <anthony@aoscovitch.com>
2017-10-10 22:22:41 -0400

    Add Domain Project

---
+++ MaintRules/Domain/Repair.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaintRules.Domain
+{
+    public class Repair
+    {
+        public int ID { get; set; }
+        public int Tenant_ID { get; set; }
+        public int Worker_ID { get; set; }
+        public int Complex_ID { get; set; }
+        public string Issue { get; set; }
+        public string IssueDetails { get; set; }
+        public string Status { get; set; }
+        public bool ActiveFlag { get; set; }
+
+
+        public Repair(int Tenant_ID, string Issue, string IssueDetails)
+        {
+            this.Tenant_ID = Tenant_ID;
+            this.Issue = Issue;
+            this.IssueDetails = IssueDetails;
+            Status = "Open";
+            ActiveFlag = true;
+        }
+
+        public Repair(int iD, int tenant_ID, int worker_ID, int complex_ID, string issue, string issueDetails, string status, bool activeFlag)
+        {
+            ID = iD;
+            Tenant_ID = tenant_ID;
+            Worker_ID = worker_ID;
+            Complex_ID = complex_ID;
+            Issue = issue;
+            IssueDetails = issueDetails;
+            Status = status;
+            ActiveFlag = activeFlag;
+        }
+
+        public void CompleteRepair()
+        {
+            Status = "Resolved";
+            ActiveFlag = false;
+        }
+    }
+}
---
+++ MaintRules/Domain/Worker.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaintRules.Domain
+{
+    public class Worker
+    {
+        public int WorkerID { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public bool ActiveFlag { get; set; }
+
+        public Worker(int workerID, string first, string last, string email, int phone, string password, string role, bool activeFlag)
+        {
+            WorkerID = workerID;
+            First = first;
+            Last = last;
+            Email = email;
+            Phone = phone;
+            Password = password;
+            Role = role;
+            ActiveFlag = activeFlag;
+        }
+
+        public Repair AssignToSelf(Repair r)
+        {
+            r.Worker_ID = WorkerID;
+            return r;
+        }
+
+        public Repair AssignToOther(Repair r, Worker w)
+        {
+            r.Worker_ID = w.WorkerID;
+            return r;
+        }
+
+        public Repair CloseRepair(Repair r)
+        {
+            r.Status = "Closed";
+            r.ActiveFlag = false;
+            return r;
+        }
+
+        public Repair ReopenRepair(Repair r)
+        {
+            r.Status = "Reopened";
+            r.ActiveFlag = true;
+            return r;
+        }
+
+        public Repair ChangeDetails(Repair r, string newdetail) 
+        {
+            r.IssueDetails = newdetail;
+            return r;
+        }
+
+        public Repair CreateRepair(Tenant t, string issue, string issuedetails) 
+        {
+            var res = new Repair(t.TenantID, issue, issuedetails);
+
+            if (ActiveFlag) 
+            {
+                return res;
+            } else {
+                return null;
+            }
+        }
+    }
+}
